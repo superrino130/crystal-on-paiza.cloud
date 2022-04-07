494453b0edbd51ff44b7baf1224dbf1e5e2f6a80
superrino130 <60283445+superrino130@users.noreply.github.com>
2022-04-07 19:41:01 +0900

    add minitest

--- Gemfile
+++ Gemfile
@@ -1,7 +1,4 @@
 source 'https://rubygems.org'
 
 # Gemspec
-gem "ruby-debug-ide"
-gem "debase"
-gem "rcodetools"
 gem "minitest-reporters"
--- init.sh
+++ init.sh
@@ -2,3 +2,4 @@ sudo systemctl enable mysql
 sudo systemctl start mysql
 curl https://dist.crystal-lang.org/apt/setup.sh | sudo bash
 sudo apt-get install crystal
+bundle config set path 'vendor/bundle'
